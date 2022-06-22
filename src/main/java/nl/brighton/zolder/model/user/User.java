fb995b54ee97b7fae751ed92697e3422bbab1396
Brighton van den End <BBT.vandenEnd@student.han.nl>
2022-06-22 12:21:49 +0200

    Update User.java
    added clear password method

--- src/main/java/nl/brighton/zolder/model/user/User.java
+++ src/main/java/nl/brighton/zolder/model/user/User.java
@@ -1,11 +1,6 @@
 package nl.brighton.zolder.model.user;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.DBRef;
@@ -21,18 +16,22 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @CompoundIndex(name = "username-index", def = "{'username':1}", unique = true)
 public class User {
 
-  @Id
-  private String id;
-  @EqualsAndHashCode.Include
-  private String username;
-  @EqualsAndHashCode.Include
-  private String password;
-  private boolean isActive = false;
-  @DBRef
-  private UserRoleType role;
+    @Id
+    private String id;
+    @EqualsAndHashCode.Include
+    private String username;
+    @EqualsAndHashCode.Include
+    private String password;
+    private boolean isActive = false;
+    @DBRef
+    private UserRoleType role;
 
-  public boolean hasSameID(User user) {
-    return this.id.equals(user.id);
-  }
+    public boolean hasSameID(User user) {
+        return this.id.equals(user.id);
+    }
+
+    public void clearPassword() {
+        this.password = null;
+    }
 }
 
